a1980314bcb7414be0084ca226649aee53d8980a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 18:43:05 +0000

    Deploy Microsoft/BotFramework-WebChat to github.com/Microsoft/BotFramework-WebChat.git:gh-pages

---
+++ breakaway-speech-button/precache-manifest.95d614b42827180cbeaffb28ce5b5bdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7936e5b16c017c22e0c",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/static/js/runtime~main.d7936e5b.js"
+  },
+  {
+    "revision": "8e1133ea056c147c4bf7",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/static/js/main.8e1133ea.chunk.js"
+  },
+  {
+    "revision": "8473a17e5e8c36cd023c",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/static/js/1.8473a17e.chunk.js"
+  },
+  {
+    "revision": "8e1133ea056c147c4bf7",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/static/css/main.a2553ad1.chunk.css"
+  },
+  {
+    "revision": "e4f8dfc80a657256ad81e5704eb211f2",
+    "url": "/BotFramework-WebChat/breakaway-speech-button/index.html"
+  }
+];
---
+++ minimize-mode/precache-manifest.437d980135ad8d75ec690526f29167a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d7485af88eec33605ea6ff11ecbddf",
+    "url": "/BotFramework-WebChat/minimize-mode/static/media/WebPage.d6d7485a.jpg"
+  },
+  {
+    "revision": "d57f93c0e1a7dde4c2da",
+    "url": "/BotFramework-WebChat/minimize-mode/static/js/runtime~main.d57f93c0.js"
+  },
+  {
+    "revision": "5eb452cfba6012ecce1c",
+    "url": "/BotFramework-WebChat/minimize-mode/static/js/main.5eb452cf.chunk.js"
+  },
+  {
+    "revision": "4722454be6643f725ebe",
+    "url": "/BotFramework-WebChat/minimize-mode/static/js/1.4722454b.chunk.js"
+  },
+  {
+    "revision": "5eb452cfba6012ecce1c",
+    "url": "/BotFramework-WebChat/minimize-mode/static/css/main.9c90d2da.chunk.css"
+  },
+  {
+    "revision": "6abe738961cd8451963b0059c4e4c7db",
+    "url": "/BotFramework-WebChat/minimize-mode/index.html"
+  }
+];
